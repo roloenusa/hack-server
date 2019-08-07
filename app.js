a9fa71c1d2ebfdfcbe268bef35478aefc3ab496a
Juan Delgado <juan.delgado@wbgames.com>
2019-08-07 10:20:55 -0700

    More websocket apps because i needed to figure stuff out.

--- app.js
+++ app.js
@@ -102,11 +102,9 @@ app.listen(port, () =>
   console.log(`Express server is running on port: ${port}...`)
 )
 
-const getUniqueID = () => {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return s4() + s4() + '-' + s4();
-};
-
+/**
+ * OTHER
+ */
 
 function handleMessage(msg) {
   const dataFromClient = JSON.parse(msg);
@@ -123,3 +121,76 @@ function handleMessage(msg) {
   console.log(response);
   return response;
 }
+
+
+/**
+ * Additional websocket stuff
+ */
+
+
+const webSocketsServerPort = 3002;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+// Spinning the http server and the websocket server.
+const server = http.createServer();
+server.listen(webSocketsServerPort, () => 
+  console.log(`Websocket server listening on port ${webSocketsServerPort}...`)
+);
+const wsServer = new webSocketServer({
+  httpServer: server
+});
+
+// I'm maintaining all active connections in this object
+const clients = {};
+// I'm maintaining all active users in this object
+const users = {};
+
+const sendMessage = (json) => {
+  // We are sending the current data to all connected clients
+  Object.keys(clients).map((client) => {
+    clients[client].sendUTF(json);
+  });
+}
+
+// Generates unique ID for every new connection
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+
+wsServer.on('request', function(request) {
+  var userID = getUniqueID();
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      const dataFromClient = JSON.parse(message.utf8Data);
+      const json = { type: dataFromClient.type };
+      if (dataFromClient.type === typesDef.USER_EVENT) {
+        users[userID] = dataFromClient;
+        userActivity.push(`${dataFromClient.username} joined to edit the document`);
+        json.data = { users, userActivity };
+      } else if (dataFromClient.type === typesDef.CONTENT_CHANGE) {
+        editorContent = dataFromClient.content;
+        json.data = { editorContent, userActivity };
+      }
+      sendMessage(JSON.stringify(json));
+    }
+  });
+  // user disconnected
+  connection.on('close', function(connection) {
+    console.log((new Date()) + " Peer " + userID + " disconnected.");
+    const json = { type: typesDef.USER_EVENT };
+    userActivity.push(`${users[userID].username} left the document`);
+    json.data = { users, userActivity };
+    delete clients[userID];
+    delete users[userID];
+    sendMessage(JSON.stringify(json));
+  });
+});
+
+
