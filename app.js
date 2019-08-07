2cc751ab97fb2929622ec23390a8ea615fa76fc1
Juan Delgado <juan.delgado@wbgames.com>
2019-08-07 09:46:40 -0700

    Adding some websocket functionality.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var cors = require('cors')
 const express = require('express');
-const expressWs = require('express-ws');
-
 const app = express();
+const expressWs = require('express-ws')(app);
 const gameData = require('./game-data.js');
+
+const http = require('http');
+const WebSocket = require('ws');
 
 /**
  * Middlewares
@@ -15,6 +17,24 @@ app.use((req, res, next) => {
   console.log(`[${new Date().toLocaleString()}][${red}${req.method}${nc}] => ${req.originalUrl}`);
   next(); // Call the next piece of middleware.
 });
+
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({server});
+
+// wss.on('connection', (ws) => {
+//   console.log("connection");
+//   //connection is up, let's add a simple simple event
+//   ws.onmessage((message) => {
+
+//       //log the received message and send it back to the client
+//       console.log('received: %s', message);
+//       ws.send(`Hello, you sent -> ${message}`);
+//   });
+
+//   //send immediatly a feedback to the incoming connection    
+//   ws.send('Hi there, I am a WebSocket server');
+// });
+
 
 /**
  * Routes
@@ -30,6 +50,34 @@ app.get('/gamedata', (req, res) => {
   res.json(gameData);
 });
 
+const typesDef = {
+  USER_EVENT: "userevent",
+  CONTENT_CHANGE: "contentchange"
+}
+
+let editorContent = null;
+let userActivity = [];
+
+app.ws('/', function(ws, req) {
+  console.log("on connected");
+  ws.on('message', function(msg) {
+    console.log(`${req.originalUrl}: ${msg}`)
+    var message = handleMessage(msg);
+    expressWs.getWss().clients.forEach(client => {
+      client.send(message);
+    })
+  });
+});
+
+app.ws('/test', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(`${req.originalUrl}: ${msg}`)
+    expressWs.getWss().clients.forEach(client => {
+      client.send(msg);
+    })
+  });
+});
+
 /**
  * Start
  */
@@ -38,3 +86,25 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => 
   console.log(`Express server is running on port: ${port}...`)
 )
+
+const getUniqueID = () => {
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
+};
+
+
+function handleMessage(msg) {
+  const dataFromClient = JSON.parse(msg);
+  const json = { type: dataFromClient.type };
+  if (dataFromClient.type === typesDef.USER_EVENT) {
+    users[userID] = dataFromClient;
+    userActivity.push(`${dataFromClient.username} joined to edit the document`);
+    json.data = { users, userActivity };
+  } else if (dataFromClient.type === typesDef.CONTENT_CHANGE) {
+    editorContent = dataFromClient.content;
+    json.data = { editorContent: editorContent, userActivity: userActivity };
+  }
+  var response = JSON.stringify(json);
+  console.log(response);
+  return response;
+}
--- hack-client/src/App.js
+++ hack-client/src/App.js
@@ -2,10 +2,53 @@ import Styled from 'styled-components';
 import React from 'react';
 import { action, observable } from 'mobx';
 import { observer } from 'mobx-react';
+import Editor from 'react-medium-editor';
+
+
+import Identicon from 'react-identicons';
+import {
+  Navbar,
+  NavbarBrand,
+  UncontrolledTooltip
+} from 'reactstrap';
+// import 'medium-editor/dist/css/medium-editor.css';
+// import 'medium-editor/dist/css/themes/default.css';
+
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://127.0.0.1:3001');
+const contentDefaultMessage = "Start writing your document here";
 
 @observer
 class App extends React.Component {
   @observable users = []
+  @observable currentUsers = []
+  @observable userActivity = []
+  @observable text = '';
+  @observable username = 'john'
+
+  logInUser = () => {
+    const username = this.username.value;
+    if (username.trim()) {
+      this.username = username
+      client.send(
+        JSON.stringify({
+          username: this.username,
+          type: "userevent"
+        })
+      );
+    }
+  }
+
+  onEditorStateChange = (event) => {
+    console.log(event.target.value);
+    client.send(JSON.stringify({
+      type: "contentchange",
+      username: this.username,
+      content: event.target.value
+    }));
+  };
+
 
   componentDidMount() {
     fetch('http://localhost:3001')
@@ -17,21 +60,107 @@ class App extends React.Component {
     });
   }
 
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+      const dataFromServer = JSON.parse(message.data);
+      console.log(dataFromServer);
+      if (dataFromServer.type === "userevent") {
+        this.currentUsers = Object.values(dataFromServer.data.users);
+      } else if (dataFromServer.type === "contentchange") {
+        this.text = dataFromServer.data.editorContent || contentDefaultMessage;
+      }
+      console.log("== ")
+      console.log(dataFromServer);
+      this.userActivity = dataFromServer.data.userActivity;
+      console.log("== ")
+    };
+  }
+
+  showLoginSection = () => (
+    <div className="account">
+      <div className="account__wrapper">
+        <div className="account__card">
+          <div className="account__profile">
+            <Identicon className="account__avatar" size={64} string="randomness" />
+            <p className="account__name">Hello, user!</p>
+            <p className="account__sub">Join to edit the document</p>
+          </div>
+          <input name="username" ref={(input) => { this.username = input; }} className="form-control" />
+          <button type="button" onClick={() => this.logInUser()} className="btn btn-primary account__btn">Join</button>
+        </div>
+      </div>
+    </div>
+  )
+
+
   getUserNames = () => {
     return this.users.map(user =>{
       return <li key={user.id}>{user.username}</li>
     })
   }
 
-  render() {
-    const result = this.getUserNames();
-    return (
-      <div className="App">
-        <h1>Users</h1>
+  showEditorSection = () => (
+    <div className="main-content">
+      <div className="document-holder">
+        <div className="currentusers">
+          {this.currentUsers.map(user => (
+            <React.Fragment>
+              <span id={user.username} className="userInfo" key={user.username}>
+                <Identicon className="account__avatar" style={{ backgroundColor: user.randomcolor }} size={40} string={user.username} />
+              </span>
+              <UncontrolledTooltip placement="top" target={user.username}>
+                {user.username}
+              </UncontrolledTooltip>
+            </React.Fragment>
+          ))}
+        </div>
+        {/* <Editor
+          options={{
+            placeholder: {
+              text: this.text ? contentDefaultMessage : ""
+            }
+          }}
+          className="body-editor"
+          text={this.text}
+          onChange={this.onEditorStateChange}
+        /> */}
+
+        <textarea value={this.text} onChange={this.onEditorStateChange} />
+      </div>
+      <div className="history-holder">
         <ul>
-          { result }
+          {this.userActivity.map((activity, index) => <li key={`activity-${index}`}>{activity}</li>)}
         </ul>
       </div>
+    </div>
+  )
+
+  // render() {
+  //   const result = this.getUserNames();
+  //   return (
+  //     <div className="App">
+  //       <h1>Users</h1>
+  //       <ul>
+  //         { result }
+  //       </ul>
+  //       <div>
+  //         { this.showEditorSection() }
+  //       </div>
+  //     </div>
+  //   );
+  // }
+  render() {
+    const username = this.username;
+    return (
+      <React.Fragment>
+        <div className="container-fluid">
+          {this.showEditorSection()}
+        </div>
+      </React.Fragment>
     );
   }
 }
