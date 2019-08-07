aa4391ef1b2a52609615cd7e19c5e1c753c01c1f
Juan Delgado <juan.delgado@wbgames.com>
2019-08-07 15:09:12 -0700

    Character creation.

--- hack-client/src/App.js
+++ hack-client/src/App.js
@@ -18,6 +18,7 @@ import {
 
 
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { thisExpression } from '@babel/types';
 
 const client = new W3CWebSocket('ws://127.0.0.1:3001');
 const client2 = new W3CWebSocket('ws://127.0.0.1:3001/test');
@@ -43,17 +44,19 @@ const Input = Styled.input`
 
 @observer
 class App extends React.Component {
-  @observable game = null
-  @observable users = []
+  @observable game = null;
+  @observable users = [];
   @observable gameState = {};
+  @observable gameData = {};
 
-  gameData = {}
+  @observable currentCharacter = {}
 
   componentWillMount() {
     fetch("http://localhost:3001/gamedata")
     .then(res => res.json())
     .then((result) => {
-        this.gameData = result;
+        console.log(result);
+        this.setGameData(result);
       },
       // Note: it's important to handle errors here
       // instead of a catch() block so that we don't swallow
@@ -69,7 +72,7 @@ class App extends React.Component {
 
     client.onmessage = (message) => {
       const dataFromServer = JSON.parse(message.data);
-      this.setGameData(dataFromServer); 
+      this.setGameState(dataFromServer); 
     };
   }
 
@@ -84,15 +87,29 @@ class App extends React.Component {
     }));
   }
 
-  @action setGameData = (data) => {
+  @action setGameState = (data) => {
     this.gameState = data
+  }
+
+  @action setGameData = (data) => {
+    console.log('-- data')
+    console.log(data.strengths);
+    this.gameData = data
   }
 
   findBattleUI = () => {
     return (
-    <Button onClick={this.findBattle} className="btn btn-primary">
-      Find a battle
-    </Button>
+      <div>
+        <H1>
+          Collabwar
+        </H1>
+        <div>
+          <input className="form-control form-control-lg" placeholder="enter a username" onChange={this.setUsername}/>
+        </div>
+        <Button onClick={this.findBattle} className="btn btn-primary">
+          Find a battle
+        </Button>
+      </div>
     );
   }
 
@@ -106,6 +123,90 @@ class App extends React.Component {
     return (
       <h1>Join... {this.gameState.gamedata.statedata.time}</h1>
     );
+  }
+
+  optionsMap = (type, selectList) => {
+    return (
+    <select className="form-control form-control-lg">
+      <option>{type}</option>
+      {
+        selectList.map((e, i) => 
+          <option value={i}>{e.title}</option>
+        )
+      }
+    </select>
+    )
+  }
+
+  createCharacterUI = () => {
+    if (this.gameData.strengths) {
+      return (
+        <div className="row">
+          <div className="col">
+            <h2>Make Your Character</h2>
+            <div className="char-creation">
+
+              <div className="form-group">
+              {
+                this.gameData.strengths ? this.optionsMap('strengths', this.gameData.strengths) : ''
+              }
+              </div>
+              <div className="form-group">
+              {
+                this.gameData.weaknesses ? this.optionsMap('weaknesses', this.gameData.weaknesses) : ''
+              }
+              </div>
+              <div className="form-group">
+              {
+                this.gameData.elements ? this.optionsMap('elements', this.gameData.elements) : ''
+              }
+              </div>
+              <div className="row">
+                <h3>Pick your character image</h3>
+                {
+                  this.gameData.icons.map((e) => {
+                    return (
+                      <div className="col-4 char-imgs">
+                        <button>
+                          <img src={"http://localhost:3001/img/characters/" + e + ".png"} />
+                        </button>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+
+              <div className="row">
+                <Button onClick="" className="col-12 btn btn-primary">Lock in</Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="col">
+            <h2>Your Team</h2>
+            {
+              !this.gameState.player ? '' : this.gameState.player.characters.map((e) => {
+                <div className="row">
+                  <div className="col-3">
+                    <img src={"http://localhost:3001/img/characters/" + e.icon + ".png"} />
+                  </div>
+                  <div className="col-3">
+                    <div>THE ELEMENT</div>
+                    <div>THE NAME</div>
+                    <div>THE STRENGTH</div>
+                    <div>THE THE WEAKNESS</div>
+                  </div>
+                </div>
+              })
+            }
+          </div>
+
+          <div className="col">
+            <h2>{"BLAH"}'s Team</h2>
+          </div>
+        </div>
+      );
+    }
   }
 
   renderGameState = () => {
@@ -129,15 +230,10 @@ class App extends React.Component {
         <React.Fragment>
           <div className="row flex-fill">
             <div className="col align-self-center content">
-              <H1>
-                Collabwar
-              </H1>
-              <div>
-                <input placeholder="enter a username" onChange={this.setUsername}/>
-              </div>
               <div>
               {
-                this.renderGameState()
+                // this.renderGameState()
+                this.createCharacterUI()
               }
               </div>
             </div>
